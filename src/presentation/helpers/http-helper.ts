5c860786c42ff687db4ae87bfaf9f909fa156741
Lucas Gusmão de Freitas <Lucaslgf2@yahoo.com.br>
2020-10-11 23:28:41 -0300

    refactor: mover codigo duplicado para um metodo helper

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,15 @@
+import { HttpResponse } from '../protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error
+  }
+}
+
+/*
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+*/
