ad21c9883af92fda06aaeeb4d64f16cb192fd5cf
Lucas Gusmão de Freitas <Lucaslgf2@yahoo.com.br>
2020-10-11 23:35:24 -0300

    test: garantir que o 'SignUpController' retorne 400 e msg de erro se nenhum password for enviado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,9 +16,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
-})
 
-describe('SignUp Controller', () => {
   test('Deve retornar 400 se nenhum email for fornecido', () => {
     // sut = System Under Test
     const sut = new SignUpController()
@@ -32,5 +30,35 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Deve retornar 400 se nenhum password for fornecido', () => {
+    // sut = System Under Test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Deve retornar 400 se nenhum passwordConfirmation for fornecido', () => {
+    // sut = System Under Test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
