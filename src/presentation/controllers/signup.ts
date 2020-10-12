1ae2590806a8a40f558ffaaabbbdf4e5cb78d10b
Lucas Gusmão de Freitas <Lucaslgf2@yahoo.com.br>
2020-10-11 23:37:48 -0300

    feat: garantir que o 'SignUpController' retorne 400 se nenhum passwordConfirmation for enviado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const camposObrigatorios = ['name', 'email', 'password']
+    const camposObrigatorios = ['name', 'email', 'password', 'passwordConfirmation']
     for (const campo of camposObrigatorios) {
       if (!httpRequest.body[campo]) {
         return badRequest(new MissingParamError(campo))
